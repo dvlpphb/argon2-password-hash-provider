a698577192eb847a3fd48ede5bd7b6c7ce1d54d1
David Lippi <david.lippi@hibo.it>
2023-01-04 11:55:59 +0100

    upgrade to keycloak 20.0.2

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-docker run -it --rm --name argon2-password-hash-provider -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 mvn clean package
-echo "cp target/argon2-password-hash-provider-2.0.1.jar [your]/keycloak/deployments/"
+docker run -it --rm --name argon2-password-hash-provider -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.8.7 mvn clean package
+echo "cp target/argon2-password-hash-provider-20.0.2.jar [your]/keycloak/deployments/"
 #cp target/argon2-password-hash-provider-2.0.1.jar ../../../../dxp-services/keycloak/deployments/
