f102e442523f968bdfe7a0cc35476438d0f70112
David Lippi <david.lippi@hibo.it>
2023-01-16 13:15:40 +0100

    variable for keycloak version

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+KEYCLOAK_VERSION="20.0.3"
+
 docker run -it --rm --name argon2-password-hash-provider -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.8.7 mvn clean package
-echo "cp target/argon2-password-hash-provider-20.0.3.jar [your]/keycloak/deployments/"
-#cp target/argon2-password-hash-provider-20.0.3.jar ../../../../dxp-services/keycloak/deployments/
+echo "cp target/argon2-password-hash-provider-${KEYCLOAK_VERSION}.jar [your]/keycloak/deployments/"
+#cp target/argon2-password-hash-provider-${KEYCLOAK_VERSION}.jar ../../../../dxp-services/keycloak/deployments/
