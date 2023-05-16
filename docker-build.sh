0af1a8578d4a38f401a68423a2a03e2d90c738d9
David Lippi <david.lippi@hibo.it>
2023-05-16 15:07:14 +0200

    Upgraded to keycloak 21.1.1

--- docker-build.sh
+++ docker-build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-KEYCLOAK_VERSION="20.0.3"
+KEYCLOAK_VERSION="21.1.1"
 
 docker run -it --rm --name argon2-password-hash-provider -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.8.7 mvn clean package
 echo "cp target/argon2-password-hash-provider-${KEYCLOAK_VERSION}.jar [your]/keycloak/deployments/"
